c364e4b7b7044039eb98aae0dc231f6ec778be84
kvynlim <37014222+kvynlim@users.noreply.github.com>
2020-09-22 18:28:36 +0800

    Create buttons.ino

---
+++ buttons.ino
@@ -0,0 +1,25 @@
+volatile byte state = LOW;
+
+void setup() {
+//  Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), blink, FALLING);
+}
+
+void loop() {pwm_sbus();}
+
+void pwm_sbus() {
+  analogWrite(10, 110);
+  analogWrite(11, 110);
+  
+  if (state == HIGH) {
+    analogWrite(9, 250); 
+  }
+  else if (state == LOW) {
+    analogWrite(9, 110);
+  }
+}
+
+void blink() {
+  state = !state;
+}
